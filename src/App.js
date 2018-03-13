4822f5dd1d6fb77544450952e1fd6b419994425a
Mateo Olarte <mateo.olarte8@gmail.com>
2018-03-13 03:16:30 -0500

    add filter to posts

--- src/App.js
+++ src/App.js
@@ -2,20 +2,49 @@ import React, { Component } from 'react';
 import posts from './posts'
 
 
-// Modifica el componente App para implmentar la funcionalidad requerida
+// Modifica el componente App para implementar la funcionalidad requerida
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      initialPosts: posts,
+      filterPosts: []
+    }
+
+    this.updatedPosts = this.updatedPosts.bind(this)
+  }
+
+  updatedPosts(e) {
+    let updatedPosts = this.state.initialPosts
+
+    updatedPosts = updatedPosts.filter(post =>
+      post.title.toLowerCase().search(e.target.value.toLowerCase()) !== -1
+    )
+
+    this.setState({
+      filterPosts: updatedPosts
+    })
+  }
+
+  componentWillMount() {
+    this.setState({ filterPosts: this.state.initialPosts })
+  }
+
   render() {
     return (
       <div>
         <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
+          <input type="text" placeholder="Ingresa el término de búsqueda" onChange={this.updatedPosts} />
         </div>
         <ul>
-          <li>
-            <a href={posts[0].url}><img src={posts[0].image } /></a>
-            <p>{ posts[0].title }</p>
-          </li>
+          { this.state.filterPosts.map((post, i) => 
+            <li key={i}>
+              <a href={post.url}><img src={post.image} alt={ post.title } /></a>
+              <p>{ post.title }</p>
+            </li>
+          ) }
         </ul>
       </div>
     )
